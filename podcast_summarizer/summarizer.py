8fc953b0e9dd475e7cbaf3af7fb8e07545d0b148
Daniel Noguchi <2511352+dnng@users.noreply.github.com>
2023-07-14 02:06:16 -0300

    Run python's black code formatter and add logging

--- podcast_summarizer/summarizer.py
+++ podcast_summarizer/summarizer.py
@@ -3,10 +3,14 @@ import openai
 import urllib.request
 import tempfile
 import whisper
+import logging
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 
 messages = []
-SUPPORTED_AUDIO_FORMATS = ["m4a","mp3","webm","mp4","mpga","wav","mpeg"]
+SUPPORTED_AUDIO_FORMATS = ["m4a", "mp3", "webm", "mp4", "mpga", "wav", "mpeg"]
+
 
 def openAudio(url_or_path, audio_format):
     if os.path.isfile(url_or_path):
@@ -24,30 +28,44 @@ def openAudio(url_or_path, audio_format):
     except ValueError:
         raise Exception("Invalid URL or path.")
 
+
 def transcribe(mp3_path):
     model = whisper.load_model("tiny")
     result = model.transcribe(mp3_path, fp16=False)
 
     return result["text"]
 
+
 def summarize(text, language):
-    return askLLM(f'Please summarize the following podcast transcription, respond in "{language}": {text}')
+    return askLLM(
+        f'Please summarize the following podcast transcription, respond in "{language}": {text}'
+    )
+
 
 def askLLM(message):
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    
+    if not openai.api_key:
+        logging.error("OPENAI_API_KEY not set", exc_info=True)
+
     global messages
 
-    messages += [{ 
-        "role": "user",
-        "content": message,
-    }]
+    messages += [
+        {
+            "role": "user",
+            "content": message,
+        }
+    ]
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=messages,
-        temperature=0.7,
-    )
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",
+            messages=messages,
+            temperature=0.7,
+        )
+    except openai.error.RateLimitError as e:
+        logging.error("RateLimitError occurred:", exc_info=True)
+        logging.error("RateLimitError: %s", e)
+        logging.error("Error details: %s", e.__dict__)
 
     response = completion.choices[0].message.content
     return response
